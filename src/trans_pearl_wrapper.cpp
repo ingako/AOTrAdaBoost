085eea5a833c46c743b23c301bbcb3f256ecf2cd
ingako <hwu344@aucklanduni.ac.nz>
2021-02-04 18:40:55 +1300

    Add trans_pearl_wrapper

---
+++ src/trans_pearl_wrapper.cpp
@@ -0,0 +1,96 @@
+#include "trans_pearl_wrapper.h"
+#include "trans_pearl.h"
+
+trans_pearl_wrapper::trans_pearl_wrapper(
+                    int num_classifiers,
+                    int num_trees,
+                    int max_num_candidate_trees,
+                    int repo_size,
+                    int edit_distance_threshold,
+                    int kappa_window_size,
+                    int lossy_window_size,
+                    int reuse_window_size,
+                    int arf_max_features,
+                    int lambda,
+                    int seed,
+                    double bg_kappa_threshold,
+                    double cd_kappa_threshold,
+                    double reuse_rate_upper_bound,
+                    double warning_delta,
+                    double drift_delta,
+                    int pro_drift_window_size,
+                    double hybrid_delta,
+                    int backtrack_window,
+                    double stability_delta) {
+
+    for (int i = 0; i < num_classifiers; i++) {
+        shared_ptr<trans_pearl> classifier = make_shared<trans_pearl>(
+                num_trees,
+                max_num_candidate_trees,
+                repo_size,
+                edit_distance_threshold,
+                kappa_window_size,
+                lossy_window_size,
+                reuse_window_size,
+                arf_max_features,
+                lambda,
+                seed,
+                bg_kappa_threshold,
+                cd_kappa_threshold,
+                reuse_rate_upper_bound,
+                warning_delta,
+                drift_delta,
+                pro_drift_window_size,
+                hybrid_delta,
+                backtrack_window,
+                stability_delta);
+        classifiers.push_back(classifier);
+    }
+
+    for (int i = 0; i < num_classifiers; i++) {
+        for (int j = 0; j < num_classifiers; j++) {
+            if (i == j) continue;
+            classifiers[i]->register_tree_pool(classifiers[j]->get_concept_repo());
+        }
+    }
+}
+
+void trans_pearl_wrapper::switch_classifier(int classifier_idx){
+    current_classifier = classifiers[classifier_idx];
+}
+
+void trans_pearl_wrapper::train() {
+    current_classifier->train();
+}
+
+int trans_pearl_wrapper::predict() {
+    return current_classifier->predict();
+}
+
+int trans_pearl_wrapper::get_cur_instance_label() {
+    return current_classifier->get_cur_instance_label();
+}
+
+void trans_pearl_wrapper::init_data_source(int classifier_idx, const string &filename){
+    classifiers[classifier_idx]->init_data_source(filename);
+}
+
+bool trans_pearl_wrapper::get_next_instance() {
+    return current_classifier->get_next_instance();
+}
+
+bool trans_pearl_wrapper::has_actual_drifted_trees() {
+    return current_classifier->has_actual_drifted_trees();
+}
+
+int trans_pearl_wrapper::get_candidate_tree_group_size() {
+    return current_classifier->get_candidate_tree_group_size();
+}
+
+int trans_pearl_wrapper::get_tree_pool_size() {
+    return current_classifier->get_tree_pool_size();
+}
+
+void trans_pearl_wrapper::match_concept() {
+    current_classifier->match_concept();
+}
---
+++ src/trans_pearl_wrapper.h
@@ -0,0 +1,49 @@
+#ifndef __TRANS_PEARL_WRAPPER_H__
+#define __TRANS_PEARL_WRAPPER_H__
+
+#include "trans_pearl.h"
+
+class trans_pearl_wrapper {
+public:
+
+    trans_pearl_wrapper(int num_classifiers,
+                        int num_trees,
+                        int max_num_candidate_trees,
+                        int repo_size,
+                        int edit_distance_threshold,
+                        int kappa_window_size,
+                        int lossy_window_size,
+                        int reuse_window_size,
+                        int arf_max_features,
+                        int lambda,
+                        int seed,
+                        double bg_kappa_threshold,
+                        double cd_kappa_threshold,
+                        double reuse_rate_upper_bound,
+                        double warning_delta,
+                        double drift_delta,
+                        int pro_drift_window_size,
+                        double hybrid_delta,
+                        int backtrack_window,
+                        double stability_delta);
+
+    void switch_classifier(int classifier_idx);
+
+    void train();
+    int predict();
+    int get_cur_instance_label();
+    void init_data_source(int classifier_idx, const string &filename);
+    bool get_next_instance();
+    bool has_actual_drifted_trees();
+    int get_candidate_tree_group_size();
+    int get_tree_pool_size();
+
+    void match_concept();
+
+private:
+
+    vector<shared_ptr<trans_pearl>> classifiers;
+    shared_ptr<trans_pearl> current_classifier;
+};
+
+#endif
