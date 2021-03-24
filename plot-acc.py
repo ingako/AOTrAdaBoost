4d4a5fe1ff8a552f5df5d1b8ed64dd3f9d0e35da
ingako <hwu344@aucklanduni.ac.nz>
2021-03-24 21:29:48 +1300

    Update result dirs for plot-acc.py

--- plot-acc.py
+++ plot-acc.py
@@ -22,33 +22,65 @@ reuse_rate = 0.9
 lossy_window = 100000000
 
 generator = "agrawal"
-generator_traits = "gradual"
+result_directory_prefix = f"bike-weekday-weekend"
+# result_directory_prefix = f"exp02-noise-0-0/"
+# result_directory_prefix = f"exp02-noise-0.1-0.1/"
+# result_directory_prefix = f"exp02-noise-0.2-0.1/"
+# result_directory_prefix = f"exp02-imbalance-19-91/"
 
-arf_result_path = f"{generator}/{generator_traits}"
-result_path = f"{generator}/{generator_traits}/k{kappa}-e{ed}/"
+seed=0
+least_transfer_warning_period_instances_length = 100 # [100 200 300]
+num_diff_distr_instances = 300 # [10 20 30 40 50]
+transfer_kappa_threshold = 0.1 # [0.1 0.2 0.3]
+bbt_pool_size = 10 # [10]
+transfer_gamma = 2.0
 
-least_transfer_warning_period_instances_length = 50
-instance_store_size = 500
-num_pseudo_instances = 300
-bbt_pool_size = 100
-mini_batch_size = 100
+# defaults
+eviction_interval = 1000000 # disabled
+instance_store_size = 5000 # default to 5000
 
-result_path = \
-    f"{result_path}/transfer/" \
-    f"{least_transfer_warning_period_instances_length}/{instance_store_size}/" \
-    f"{num_pseudo_instances}/{bbt_pool_size}/{mini_batch_size}/"
 
-stream_1_path = f"{result_path}/result-0-stream-0.csv"
-stream_2_path = f"{result_path}/result-0-stream-1.csv"
+# stream_1_path = f"{transfer_result_path}/no_boost/{seed}/result-stream-0.csv"
+# stream_1 = pd.read_csv(stream_1_path, index_col=0)
+# plt.plot(stream_1["accuracy"], label="stream_1", linestyle="--")
 
-stream_1 = pd.read_csv(stream_1_path, index_col=0)
-stream_2 = pd.read_csv(stream_2_path, index_col=0)
+# pearl benchmark
+# pearl_stream_2_path = f"{result_directory_prefix}/pearl/{seed}/result-stream-1.csv"
+# pearl_stream_2 = pd.read_csv(pearl_stream_2_path, index_col=0)
+# plt.plot(pearl_stream_2["accuracy"], label="pearl", linestyle="--")
 
-plt.plot(stream_1["accuracy"], label="stream_1", linestyle="--")
-plt.plot(stream_2["accuracy"], label="stream_2", linestyle='-.')
+for boost_mode in ["disable_transfer", "no_boost", "ozaboost", "tradaboost", "atradaboost"]:
+    result_directory = f"{result_directory_prefix}/{boost_mode}/"
+    if boost_mode == "disable_transfer":
+        pass
+    elif boost_mode == "no_boost":
+        result_directory = f"{result_directory}/" \
+                           f"{least_transfer_warning_period_instances_length}/{instance_store_size}/" \
+                           f"{transfer_kappa_threshold}/"
+    elif boost_mode == "ozaboost" or boost_mode == "tradaboost":
+        result_directory = f"{result_directory}/" \
+                           f"{least_transfer_warning_period_instances_length}/{instance_store_size}/" \
+                           f"{transfer_kappa_threshold}/" \
+                           f"{eviction_interval}/{num_diff_distr_instances}/{bbt_pool_size}/"
+    elif boost_mode == "atradaboost":
+        result_directory = f"{result_directory}/" \
+                           f"{least_transfer_warning_period_instances_length}/{instance_store_size}/" \
+                           f"{transfer_kappa_threshold}/" \
+                           f"{eviction_interval}/{num_diff_distr_instances}/{bbt_pool_size}/" \
+                           f"{transfer_gamma}/"
+    else:
+        print("unsupported boost mode")
+        exit(1)
+    stream_2_path = f"{result_directory}/result-stream-1.csv"
+
+    stream_2 = pd.read_csv(stream_2_path, index_col=0)
+    plt.plot(stream_2["accuracy"], label=f"stream_2 {boost_mode}", linestyle='-.')
+    # print(stream_2["time"].iloc[-1]/60)
+
 
 plt.legend()
 plt.xlabel("No. of Instances")
 plt.ylabel("Accuracy")
 
 plt.show()
+# plt.savefig('bike-result.png', bbox_inches='tight', dpi=300)
