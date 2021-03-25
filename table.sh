1c8e2ebb14a914a279f75080085d0c437e5dc730
ingako <ingako@protonmail.com>
2021-03-25 19:52:29 +1300

    Fix table.sh

--- table.sh
+++ table.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-./eval-tradaboost.py $1 $2 # exp_code boost_mode
+./eval-params.py $1 $2 # exp_code boost_mode
 
 # reverse sort by last column
 awk -F, '{print $NF,$0}' $1/gain-pro-report.txt | sort -nr -t',' | cut -f2- -d',' | column -s',' -t | less
