56572d9e0453b576ec71b721ff3b8ea51d2ec6bb
ingako <ingako@protonmail.com>
2021-03-26 00:12:35 +1300

    Add eval scripts for synthetic data

---
+++ eval-params-synth.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import math
+import pandas as pd
+import numpy as np
+from dataclasses import dataclass
+
+def is_empty_file(fpath):
+    return False if os.path.isfile(fpath) and os.path.getsize(fpath) > 0 else True
+
+base_dir = os.getcwd()
+exp_code = sys.argv[1]
+boost_mode = sys.argv[2]
+base_dir = f"{base_dir}/{exp_code}"
+
+gain_report_path = f"{base_dir}/gain-pro-report.txt"
+
+disable_acc_list = []
+for seed in range(10):
+    disable_output = f"{base_dir}/disable_transfer/{seed}/result-stream-1.csv"
+    disable_df = pd.read_csv(disable_output)
+    disable_acc_list.append(disable_df["accuracy"])
+
+cur_data_dir = f"{base_dir}/{boost_mode}/"
+
+if boost_mode == "no_boost":
+    param_strs = ["least_transfer_warning_period_instances_length",
+                  "num_diff_distr_instances",
+                  "transfer_kappa_threshold"]
+elif boost_mode == "ozaboost" or boost_mode == "tradaboost":
+    param_strs = ["least_transfer_warning_period_instances_length",
+                  "num_diff_distr_instances",
+                  "transfer_kappa_threshold",
+                  "bbt_pool_size"]
+elif boost_mode == "atradaboost":
+    param_strs = ["least_transfer_warning_period_instances_length",
+                  "num_diff_distr_instances",
+                  "transfer_kappa_threshold",
+                  "bbt_pool_size",
+                  "gamma"]
+else:
+    print("unsupported boost mode")
+    exit(1)
+
+
+gain_report_out = open(gain_report_path, "w")
+result_line = param_strs.copy()
+result_line.append("seed_count,acc-gain-std,acc-gain-mean\n")
+gain_report_out.write(",".join(result_line))
+
+print(f"evaluating {exp_code}...")
+print("evaluating params...")
+
+
+def eval_output(cur_data_dir, param_values, gain_report_out):
+
+    if len(param_values) != len(param_strs):
+        # recurse
+        params = [f for f in os.listdir(cur_data_dir) if os.path.isdir(os.path.join(cur_data_dir, f))]
+        print(f"evaluating {params}...")
+        for cur_param in params:
+            param_values.append(cur_param)
+            eval_output(f"{cur_data_dir}/{cur_param}", param_values, gain_report_out)
+            param_values.pop()
+
+    else:
+        result_list = []
+        for seed in range(10):
+            tradaboost_output = f"{cur_data_dir}/{seed}/result-stream-1.csv"
+            if is_empty_file(tradaboost_output):
+                continue
+
+            tradaboost_df = pd.read_csv(tradaboost_output)
+            tradaboost_acc = tradaboost_df["accuracy"]
+
+            tradaboost_disable_gain = 0
+            end = len(tradaboost_acc)
+            # end = 150
+
+            for i in range(0, end):
+                tradaboost_disable_gain += tradaboost_acc[i] - disable_acc_list[seed][i]
+
+                if i == (end - 1):
+                    result_list.append(tradaboost_disable_gain)
+        mean = np.mean(result_list)
+        std = np.std(result_list)
+        result_line = [f"{param_values[i]}" for i in range(len(param_strs))]
+        result_line.append(f"{len(result_list)},{std},{mean}\n")
+        gain_report_out.write(",".join(result_line))
+
+
+eval_output(cur_data_dir, [], gain_report_out)
+gain_report_out.close()
---
+++ table-synth.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+./eval-params-synth.py $1 $2 # exp_code boost_mode
+
+# reverse sort by last column
+awk -F, '{print $NF,$0}' $1/gain-pro-report.txt | sort -nr -t',' | cut -f2- -d',' | column -s',' -t | less
+
